be49ae1f159b4b0ae74936d4c061e2a72e6e53f7
Andrii <andrii.dimov@divtec.ch>
2026-08-25 14:34:43 +0200

    start

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * Fichier JavaScript pour l'application PokeCount.
+ * @author Steve Fallet <steve.fallet@divtec.ch>
+ * @version 0.1 (Version actuelle)
+ * @since 2024-01-31 (Date de création)
+ */
+
+console.log("Bonjour à tous!")
